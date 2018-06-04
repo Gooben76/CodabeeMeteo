62d39b759f40af69be59b761e746178ec6a52669
Benoît Goossens <benoitgoossens@macben.lan>
2018-06-04 20:53:23 +0200

    Mise en place du projet et infos.plist

---
+++ CodabeeMeteo/Controller/MeteoController.swift
@@ -0,0 +1,19 @@
+//
+//  MeteoController.swift
+//  CodabeeMeteo
+//
+//  Created by Benoît Goossens on 4/06/18.
+//  Copyright © 2018 Benoît Goossens. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MeteoController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
